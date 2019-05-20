5ff2877310b5ca3033a71c4dc7143a1bae5c41c9
0548643 <0548643@hr.nl>
2019-05-20 16:13:48 +0200

    Changes

--- EduNote.Mobile/src/app/pages/note/list/list.page.ts
+++ EduNote.Mobile/src/app/pages/note/list/list.page.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Note } from 'src/app/core/domains/note';
 import { NoteService } from 'src/app/api/note.service';
 import { ActivatedRoute } from '@angular/router';
+import { ModalController } from '@ionic/angular';
+import { EditPage } from '../edit/edit.page';
+import { EditNoteComponent } from '../edit/edit-note/edit-note.component';
 
 @Component({
   selector: 'app-list',
@@ -14,7 +17,8 @@ export class ListPage implements OnInit {
 
   constructor(
     private activatedRoute: ActivatedRoute,
-    private noteService: NoteService
+    private noteService: NoteService,
+    private modalCtrl: ModalController
   ) { }
 
   ngOnInit() {
@@ -22,5 +26,17 @@ export class ListPage implements OnInit {
     this.noteService.getNotesBySection(sectionId).subscribe((notes) => {
       this.notes = notes;
     });
+  }
+
+  async addNote(id: number) {
+    const modal = await this.modalCtrl.create({
+      component: EditNoteComponent,
+      componentProps: {
+        'id': null,
+        'isEdit': false
+      }
+    });
+    await modal.present();
+    const data = await modal.onDidDismiss();
   }
 }
